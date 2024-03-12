e12375ad07ee062627d9b3d1c23d243a506e9547
L3onSW <ssl3on590@gmail.com>
2024-03-13 03:08:34 +0900

    pyPCPの中身を若干変更
    関数に対するコメントの書き方を変更
    元々コメントに入れていた別の結果の表示の仕方をオプション引数で指定できるようにした

--- pyPCP.py
+++ pyPCP.py
@@ -5,10 +5,11 @@
 # Created on 2023/12/06, author: L3onSW
 # ======================================================================
 
-# ----------------------------------------------------------------------
-# ドミノを上側と下側で分割する
-# ----------------------------------------------------------------------
+
 def divide_domino(domino):
+    """divide_domino
+    ドミノを上側と下側で分割する
+    """
     domino = domino.replace("{", "").replace("}", "")
     domino = domino.replace("(", "").replace(")", "")
     domino = domino.split(",")
@@ -19,10 +20,10 @@ def divide_domino(domino):
     return upper, lower
 
 
-# ----------------------------------------------------------------------
-# ポストの対応問題(PCP)の問題文を表示する
-# ----------------------------------------------------------------------
 def print_problem_statement(upper, lower):
+    """print_problem_statement
+    ポストの対応問題(PCP)の問題文を表示する
+    """
     print("-" * 70)
     print("PCPのリスト", end="")
     print("{", end="")
@@ -36,10 +37,10 @@ def print_problem_statement(upper, lower):
     print("-" * 70)
 
 
-# ----------------------------------------------------------------------
-# それぞれの長さで発見された共通系列を表示する
-# ----------------------------------------------------------------------
 def print_discovered_match(upper, lower, domino_index, num, vertically=True):
+    """print_discovered_match
+    それぞれの長さで発見された共通系列を表示する
+    """
     # 横に並べて表示する
     print("{", end="")
     for i in range(num-1):
@@ -59,11 +60,11 @@ def print_discovered_match(upper, lower, domino_index, num, vertically=True):
         print(f"{lower[domino_index[num-1]]}", end="\n")
 
 
-# ----------------------------------------------------------------------
-# num個だけ元のペアのリストから文字列を持ってきて結合し,
-# その後2つのリストが共通系列かどうか比較する
-# ----------------------------------------------------------------------
 def string_concatenation_and_comparison(upper, lower, domino_index, num):
+    """string_concatenation_and_comparison
+    num個だけ元のペアのリストから文字列を持ってきて結合し,
+    その後2つのリストが共通系列かどうか比較する
+    """
     # string1をupperを順番に合体させて作る
     # string2をlowerを順番に合体させて作る
     string1 = ""
@@ -77,10 +78,10 @@ def string_concatenation_and_comparison(upper, lower, domino_index, num):
         print_discovered_match(upper, lower, domino_index, num)
 
 
-# ----------------------------------------------------------------------
-# 指定された長さでポストの対応問題(PCP)を再帰的に解く
-# ----------------------------------------------------------------------
 def solve_pcp_with_specified_length(upper, lower, domino_index, length, num):
+    """solve_pcp_with_specified_length
+    指定された長さでポストの対応問題(PCP)を再帰的に解く
+    """
     if length == num:
         string_concatenation_and_comparison(upper, lower, domino_index, num)
     else:
@@ -93,7 +94,16 @@ def solve_pcp_with_specified_length(upper, lower, domino_index, length, num):
 # ----------------------------------------------------------------------
 # ポストの対応問題(PCP)を指定した長さ以下で解く
 # ----------------------------------------------------------------------
-def solve_pcp(domino, match_length):
+def solve_pcp(domino, match_length, print_num=False):
+    """solve_pcp
+    ポストの対応問題(PCP)を指定した長さ以下で解く
+
+    Args:
+        domino (_type_): ドミノの文字列
+        match_length (_type_): PCPを解く最大の長さ(この長さ以下で解く)
+        print_num (bool, optional):
+          何個の文字列の組みを持ってきた場合か表示. デフォルトはFalse(非表示).
+    """
     # 元のペアのリストを分割した2つのリストを作る
     upper, lower = divide_domino(domino)
     # PCP(Post_Correspondence_Problem)の問題文を表示する
@@ -101,6 +111,7 @@ def solve_pcp(domino, match_length):
     # PCP(Post_Correspondence_Problem)を指定した長さ以下で解く
     for i in range(1, match_length+1, 1):
         domino_index = [-1] * match_length
-        # print()
-        # print(f"ドミノから{i}個の文字列の組を持ってきた場合")
+        if print_num is True:
+            print()
+            print(f"ドミノから{i}個の文字列の組を持ってきた場合")
         solve_pcp_with_specified_length(upper, lower, domino_index, i, 0)
